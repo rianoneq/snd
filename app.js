95fea34248f709ab23c70a9529942682e6c60417
itRianone <timokhindimitry@gmail.com>
2024-07-04 19:12:20 +0300

    q

--- app.js
+++ app.js
@@ -92,9 +92,9 @@ let first_name = document.createElement("p");
 let username = document.createElement("p");
 let ava = document.createElement("img");
 
-first_name.innerText = tg.initDataUnsafe.user//`${tg.initDataUnsafe.user.first_name}`
+first_name.innerText = `${tg.initDataUnsafe.user.first_name}`
 username.innerText = `@${tg.initDataUnsafe.user.username}`
-ava.href = `${tg.initDataUnsafe.user.photo_url}`
+ava.innerText = `${tg.initDataUnsafe.user.photo_url}`
 
 usercard.appendChild(first_name);
 usercard.appendChild(username);
